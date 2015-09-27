0c04673b35f5b13ace24e53ba811ac25d487beff
Ivan Kornilov <vano468@gmail.com>
2015-09-27 11:04:19 +0300

    Next

--- player.rb
+++ player.rb
@@ -1,12 +1,28 @@
 class Player
 
-  VERSION = 0.1
+  VERSION = 0.2
 
   def bet_request(game_state)
-    20
+  	#sum = game_state['players'].inject(0) { |player, acc| acc + player['bet'] }
+  	avg = game_state['pot'] / 5.0
+  	puts avg
+  	#game_state['community_cards'].size >= 3
+
+  	p game_state['players']
+
+  	40
+
+	rescue Exception => e
+		puts e
+		20
+	end
   end
 
   def showdown(game_state)
+
+  end
+
+  def rank
 
   end
 end
